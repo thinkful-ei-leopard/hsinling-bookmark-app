6efb1ceb8fddb208a6675a00152e4c3d564134e8
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-17 11:26:56 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import $ from 'jquery';
+import './index.css';
+import store from './store';
+import bookmark from './bookmark';
+import api from './api';
+
+function main(){
+  store.startPage();
+}
+
+
+
+
+
+
+
+
+$(main());
---
+++ src/store.js
@@ -0,0 +1,16 @@
+export default{
+  startPage 
+}
+
+function startPage(){
+return   `<form>
+    <button type='submit' class='startpage'> + Add Bookmark</button>
+  </form>
+  <select>Filter By Minimum Rating
+  <option value = 5 >&#11088;&#11088;&#11088;&#11088;&#11088; </option>
+  <option value = 4 >&#11088;&#11088;&#11088;&#11088; </option>
+  <option value = 3 >&#11088;&#11088;&#11088; </option>
+  <option value = 2 >&#11088;&#11088; </option>
+  <option value = 1 >&#11088; </option>
+  </select>`; 
+}
